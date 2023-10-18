e959b6cdce918e978d495690a1f85b7706dac9c3
Gil C <a348503@uach.mx>
2023-10-17 21:57:20 -0600

    Se terminan los controladores de  movies y actors

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {Movie, Actor} = require('../db');
 const director = require('../models/director');
-
+const actor = require('../models/actor');
 
 function create (req, res, next) {
     const title = req.body.title;
@@ -22,21 +22,48 @@ function list (req, res, next) {
         .catch(err => res.send(err));
 }
 
-function index (req, res, next) {
-    res.send('Users index');
+function index(req, res, next){
+    const id = req.params.id;
+    Movie.findByPk(id)
+        .then(object => res.json(object))
+        .catch(err => res.send(err));
 }
 
-
-function replace (req, res, next) {
-    res.send('Users replace');
+function replace(req, res, next){
+    const id = req.params.id;
+    Movie.findByPk(id)
+        .then(object => {
+            const title = req.body.title ? req.body.title : "";
+            const genreId = req.body.genreId ? req.body.genreId : "";
+            const directorId = req.body.directorId ? req.body.directorId : "";
+            object.update({
+                title : title,
+                genreId : genreId,
+                directorId : directorId
+            }).then(obj => res.json(obj))
+            .catch(err => res.send(err));
+        }).catch(err => res.send(err));
 }
 
-function update (req, res, next) {
-    res.send('Users update');
+function update(req, res, next){
+    const id = req.params.id;
+    Movie.findByPk(id).then(object => {
+        const title = req.body.title ? req.body.title : object.title;
+        const genreId = req.body.genreId ? req.body.genreId : object.genreId;
+        const directorId = req.body.directorId ? req.body.directorId: object.directorId;
+        object.update({
+            title : title,
+            genreId : genreId,
+            directorId : directorId 
+        }).then(obj => res.json(obj)).catch(err => res.send(err));
+    }).catch(err => res.send(err));
 }
 
-function destroy (req, res, next) {
-    res.send('Users destroy');
+function destroy(req, res, next){
+    const id = req.params.id;
+    Movie.destroy({ where: {id: id} })
+            .then(object => res.json(object))
+            .catch(err => res.send(err));
 }
 
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,62 @@
 const express = require('express');
+const { User } = require('../db');
 
-
-function create (req, res, next) {
-    res.send('Users create');
+function create(req, res, next){
+    const name = req.body.name;
+    const lastName = req.body.lastName;
+    User.create({
+        name: name, 
+        lastName: lastName
+    }).then(object => res.json(object))
+    .catch(err => res.send(err));
 }
 
-function list (req, res, next) {
-    res.send('Users list');
+function list(req, res, next) {
+    User.findAll()
+            .then(objects => res.json(objects))
+            .catch(err => res.send(err));
 }
 
-function index (req, res, next) {
-    res.send('Users index');
+function index(req, res, next){
+    const id = req.params.id;
+    User.findByPk(id)
+            .then(object => res.json(object))
+            .catch(err => res.send(err));
 }
 
-
-function replace (req, res, next) {
-    res.send('Users replace');
+function replace(req, res, next){
+    const id = req.params.id; // Por la url
+    User.findByPk(id)
+            .then(object =>{
+                const name = req.body.name ? req.body.name : "";
+                const lastName = req.body.lastName ? req.body.lastName : "";
+                object.update({
+                    name : name, 
+                    lastName : lastName
+                }).then(obj => res.json(obj))
+                  .catch(err => res.send(err));
+            }).catch(err => res.send(err));
 }
 
-function update (req, res, next) {
-    res.send('Users update');
+function update(req, res, next){
+    const id = req.params.id;
+    User.findByPk(id).then(object => {
+        const name = req.body.name ? req.body.name : object.name;
+        const lastName = req.body.lastName ? req.body.lastName : object.lastName;
+        object.update({
+            name : name, 
+            lastName : lastName
+        }).then(obj => res.json(obj)).catch(err => res.send(err));
+    }).catch(err => res.send(err));
 }
 
-function destroy (req, res, next) {
-    res.send('Users destroy');
+function destroy(req, res, next){
+    const id = req.params.id;
+    User.destroy({ where: {id: id} })
+            .then(object => res.json(object))
+            .catch(err => res.send(err));
 }
 
-module.exports = {list, index, create, replace, update, destroy};
+module.exports = {
+    create, list, index, replace, update, destroy
+};
